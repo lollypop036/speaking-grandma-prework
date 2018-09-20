870739cca7ebd452e40598f83d7ab1ef25094d41
Lola Onanuga <slonanuga@gmail.com>
2018-09-20 16:17:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,7 @@ return "HUH?! SPEAK UP, SONNY!"
 elsif  phrase == phrase.upcase
 return "NO, NOT SINCE 1938!"
 
-elsif  phrase == "I LOVE YOU GRANDMA!" 
+elsif  phrase == "I LOVE YOU GRANDMA!" && phrase.upcase
 return "I LOVE YOU TOO PUMPKIN!"
 
 end
