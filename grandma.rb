11463c6818431b63c74f355e292585db67a2375a
Lola Onanuga <slonanuga@gmail.com>
2018-09-20 16:10:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,8 @@ if phrase != phrase.uppercase
 elsif  phrase == phrase.uppercase
 return "NO, NOT SINCE 1938!"
 
-elsif return 
+elsif  phrase == "I LOVE YOU GRANDMA!" 
+return "I LOVE YOU TOO PUMPKIN!"
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
