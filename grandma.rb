2b18207d488b21d6567b856db4827f022a0b5ad4
Lola Onanuga <slonanuga@gmail.com>
2018-09-20 16:19:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,12 +5,13 @@ def speak_to_grandma(phrase)
 if phrase != phrase.upcase
   
 return "HUH?! SPEAK UP, SONNY!"
-  
-elsif  phrase == phrase.upcase
-return "NO, NOT SINCE 1938!"
 
 elsif  phrase == "I LOVE YOU GRANDMA!" && phrase.upcase
 return "I LOVE YOU TOO PUMPKIN!"
+
+
+elsif  phrase == phrase.upcase
+return "NO, NOT SINCE 1938!"
 
 end
 end
